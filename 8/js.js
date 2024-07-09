a7c2d34802bd8478d5bee76b747dd8b0777db6d8
Robert Wyszynski <robertointerwento@gmail.com>
2024-07-09 17:15:39 +0200

    practice

--- 8/js.js
+++ 8/js.js
@@ -5,33 +5,65 @@ let ulList
 let newTodo
 
 const main = () => {
-    prepareDOMElements()
-    prepareDOMEvents()
-
+    prepareElements()
+    prepareEvents()
 }
 
-const prepareDOMElements = () => {
+const prepareElements = () => {
     todoInput = document.querySelector('.todo-input')
     errorInfo = document.querySelector('.error-info')
     addBtn = document.querySelector('.btn-add')
-    ulList = document.querySelector('.todolist ul')
+    ulList = document.querySelector('.todolist ul') // brak
 }
 
-const prepareDOMEvents = () => {
-    addBtn.addEventListener('click', addNewTask)
+const prepareEvents = () => {
+    addBtn.addEventListener('click', addTask)
+    ulList.addEventListener('click', checkClick)
 }
 
-const addNewTask = () => {
+const addTask = () => {
     if (todoInput.value !== '') {
         newTodo = document.createElement('li')
+
         newTodo.textContent = todoInput.value
+        createToolsArea()
         ulList.append(newTodo)
 
 
+        todoInput.value = ''
     } else {
-        errorInfo.textContent = 'Nie może być pusty'
+        errorInfo.textContent = 'Nic nie podałeś'
     }
 }
+
+const createToolsArea = () => {
+
+    const toolPanel = document.createElement('div')
+    toolPanel.classList.add('tools')
+    newTodo.append(toolPanel)
+
+    const okBtn = document.createElement('button')
+    okBtn.classList.add('complete')
+    okBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+    const editBtn = document.createElement('button')
+    editBtn.classList.add('edit')
+    editBtn.textContent = 'EDIT'
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('delete')
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+
+    toolPanel.append(okBtn, editBtn, deleteBtn)
+}
+
+
+
+const checkClick = e => {
+    console.log(e.target)
+}
+
+
 
 
 
